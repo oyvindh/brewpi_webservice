da4121d7516a017408250457847060c4073c6a9f
Guillaume Libersat <glibersat@sigill.org>
2016-03-29 16:39:41 +0200

    add support for BDD testing

--- brewpi_webservice/settings.py
+++ brewpi_webservice/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'behave_django',
     # Third-party libraries
     'django_extensions',
     'rest_framework',
