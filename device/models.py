a2e5dbe140a69a1c963ebb9b4a8c3113ff2b7075
Guillaume Libersat <glibersat@sigill.org>
2016-05-25 13:30:35 +0200

    rework device hierarchy to account for manual actuators

--- device/models.py
+++ device/models.py
@@ -12,22 +12,33 @@ from controller.models import Controller
 
 class Device(PolymorphicModel):
     """
-    Any hardware device (sensor, actuator) that is connected to a Controller
+    Any hardware device such as a Temperature Sensor or a Valve
     """
-    label = models.CharField(max_length=255, blank=True, null=True)
+    label = models.CharField(max_length=255, default="Unnamed Device", blank=True, null=True)
+
+    def __str__(self):
+        return "{0}: {1}".format(self.__class__.__name__, self.label)
+
+
+class ElectronicDeviceMixin(models.Model):
+    """
+    A mixin meaning the device (sensor, actuator) is operated by electricity
+    and is connected to a Controller.
+    """
+    class Meta:
+        abstract = True
+
     uri = models.CharField(max_length=255, help_text=_("A device URI such as onewire://182377282"))
     slot = models.PositiveIntegerField(blank=True, null=True,
                                        help_text=_("The slot ID assigned paired with the Controller"))
-    controller = models.ForeignKey(Controller, related_name='devices')
+    controller = models.ForeignKey(Controller, related_name='%(class)s')
 
     @property
     def is_installed(self):
         return self.slot is not None
 
-    def __str__(self):
-        return self.label or "Unnamed device (uri={0})".format(self.uri)
 
-
+### Sensors  ---------------------------------------------------------
 class Sensor(Device):
     """
     A sensor is an object whose purpose is to detect events or changes
@@ -36,15 +47,16 @@ class Sensor(Device):
     pass
 
 
+### Actuators --------------------------------------------------------
 class Actuator(Device):
     """
-    An actuator is a type of motor that is responsible for moving or
+    An actuator is a type of device that is responsible for moving or
     controlling a mechanism or system.
     """
     pass
 
 
-class PWMActuator(Actuator):
+class PWMActuatorMixin(models.Model):
     """
     An actuator that is driven by Pulse Width Modulation (PWM)
     """
