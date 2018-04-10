2d759dc4cbf14768cdd7e167b2f4310aca5f69a8
Øyvind Hvamstad <oyvind@hvamstad.net>
2018-04-10 22:15:43 +0200

    Update requirements and make swagger work

--- brewpi_webservice/urls.py
+++ brewpi_webservice/urls.py
@@ -9,11 +9,15 @@ router = routers.DefaultRouter()
 router.register(r'auth/users', auth_views.UserViewSet)
 router.register(r'auth/groups', auth_views.GroupViewSet)
 
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='BrewPI ReST API')
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^docs/', schema_view),
     url(r'^admin/', admin_site.urls),
 ]
